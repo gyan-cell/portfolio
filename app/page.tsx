1ede2448aade78bff5de50c0019dd227760c2666
gyan-cell <jha07944@gmail.com>
2024-10-07 17:51:56 +0530

    Will Finish Project Part Later

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import About from "@/components/server/About/About";
 import Hero from "@/components/server/Heropage/Hero";
+import Projects from "@/components/server/projects/Projects";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <Projects />
     </>
   )
 }
---
+++ components/server/projects/Projects.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./projects.scss"
+import { ProjectTabs } from './ProjectTab'
+
+const Projects = () => {
+  return (
+    <>
+      <div id="projects"  >
+      </div>
+    </>
+  )
+}
+
+export default Projects
