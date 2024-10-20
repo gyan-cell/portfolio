81967ccfc99681bf08f59fe436ed7954985ec0e7
gyan-cell <jha07944@gmail.com>
2024-10-20 12:40:10 +0530

    Finished The project Section

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["https://github.com/"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "github.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  }
+};
 
 export default nextConfig;
