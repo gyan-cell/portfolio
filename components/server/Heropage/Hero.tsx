0e352b96f3ae3688fc3cd9307bce724dffa92528
gyan-cell <jha07944@gmail.com>
2024-10-24 20:06:33 +0530

    Adding Remaining Stuff

--- components/server/Heropage/Hero.tsx
+++ components/server/Heropage/Hero.tsx
@@ -17,12 +17,12 @@ const Hero = () => {
         alt="hero image example"
       />
 
-      <div className="heroContent">
+      <div className={`heroContent ${itim.className}  `}>
         <div className={`heroText ${itim.className}`}  >
-          <h1 className={itim.className}  >Hi, I'm <span className='underline underline-offset-4 underline-accent'  >Gyanranjan Jha.</span></h1>
-          <h1 className={itim.className}  >I Am a <span className='underline underline-offset-4 underline-accent'  >Frontend Web Developer.</span></h1>
-          <h1 className={itim.className}  >I Am a <span className='underline underline-offset-4 underline-accent'  >Backend Developer.</span></h1>
-          <h1 className={itim.className}  >I Am a <span className='underline underline-offset-4 underline-accent'  >Machine Learning Enthusiast.</span></h1>
+          <h1 className={`itim.className`}  >Hi, I'm <span className={`underline underline-offset-4 underline-accent`}  >Gyanranjan Jha.</span></h1>
+          <h1 className={`itim.className`}  >I Am a <span className={`underline underline-offset-4 underline-accent`}  >Frontend Web Developer.</span></h1>
+          <h1 className={`itim.className`}  >I Am a <span className={`underline underline-offset-4 underline-accent`} >Backend Developer.</span></h1>
+          <h1 className={`itim.className`}  >I Am a <span className={`underline underline-offset-4 underline-accent`}  >Machine Learning Enthusiast.</span></h1>
         </div>
         <div className='heroSlider'  >
           <LottieFiles />
