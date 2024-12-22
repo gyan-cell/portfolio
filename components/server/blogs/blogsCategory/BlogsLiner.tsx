b4c7afc4d5c6b5e854cce97c81960dfacbfbe24f
gyan-cell <jha07944@gmail.com>
2024-12-22 20:44:52 +0530

    Blogs Section on Work

--- components/server/blogs/blogsCategory/BlogsLiner.tsx
+++ components/server/blogs/blogsCategory/BlogsLiner.tsx
@@ -29,20 +29,20 @@ const BlogsLiner = () => {
   return (
     <div id="blogsLiner" className="blogsLiner">
       {blogsTopic.map((blog) => (
-        <div className="blogCard">
-          <div className="blogText">
-            <div className="blogTextHead">
-              <span className={caveat.className}>{blog.title}</span>
-              <span
-                className={caveat.className + " w-full flex justify-center  "}
-              >
-                By Albert Camus
-              </span>
-              <Button className="blogButton">
-                {" "}
-                <Link href={blog.Link}>
-                  <ArrowDown />
-                </Link>
+        <div className="blogCardContainer">
+          <div className="blogCard">
+            <div className="blogCard__title">
+              <h1 className={caveat.className}>{blog.title}</h1>
+            </div>
+            <div className="blogCard__description">
+              <p className={caveat.className}>{blog.description}</p>
+            </div>
+            <div className="blogCard__category">
+              <p className={caveat.className}>{blog.category}</p>
+            </div>
+            <div className="blogCard__button">
+              <Button className={caveat.className}>
+                <Link href={`${blog.category}/${blog.Link}`}>Read More</Link>
               </Button>
             </div>
           </div>
