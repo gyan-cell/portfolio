565472c1833e8599c3829ef4987775223680a76c
gyan-cell <jha07944@gmail.com>
2025-01-09 09:18:22 +0530

    Adding NextSSR pluging to uploadthing

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ import Navbar from "@/components/server/Navbar/Navbar";
 import Footer from "@/components/server/Footer/Footer";
 import connectDb from "@/utils/connectDb";
 import { SessionProvider } from "next-auth/react";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "./api/uploadthing/core";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -35,6 +38,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NextSSRPlugin
+          /**
+           * The `extractRouterConfig` will extract **only** the route configs
+           * from the router to prevent additional information from being
+           * leaked to the client. The data passed to the client is the same
+           * as if you were to fetch `/api/uploadthing` directly.
+           */
+          routerConfig={extractRouterConfig(ourFileRouter)}
+        />
         <SessionProvider>
           <ThemeProvider
             attribute="class"
